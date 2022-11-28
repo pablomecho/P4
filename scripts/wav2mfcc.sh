152f90debb367975a7517b5fe3626b9215731677
alexsoler9 <alex.soler.baca@estudiantat.upc.edu>
2022-11-28 12:08:20 +0100

    scripts wav2

---
+++ scripts/wav2mfcc.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Make pipeline return code the last non-zero one or zero if all the commands return zero.
+set -o pipefail
+
+## \file
+## \TODO This file implements a very trivial feature extraction; use it as a template for other front ends.
+## 
+## Please, read SPTK documentation and some papers in order to implement more advanced front ends.
+
+# Base name for temporary files
+base=/tmp/$(basename $0).$$  #$$ indica el proceso
+
+# Ensure cleanup of temporary files on exit
+trap cleanup EXIT
+cleanup() {
+   \rm -f $base.*
+}
+
+if [[ $# != 4 ]]; then #Asegura el numero de parametros para la función
+   echo "$0 mfcc_order nfilter input.wav output.mfcc"
+   exit 1
+fi
+
+mfcc_order=$1
+nfilter=2$
+inputfile=$3
+outputfile=$4
+
+UBUNTU_SPTK=1
+if [[ $UBUNTU_SPTK == 1 ]]; then
+   # In case you install SPTK using debian package (apt-get)
+   X2X="sptk x2x"
+   FRAME="sptk frame"
+   WINDOW="sptk window"
+   MFCC="sptk mfcc"
+else
+   # or install SPTK building it from its source
+   X2X="x2x"
+   FRAME="frame"
+   WINDOW="window"
+   MFCC="mfcc"
+fi
+
+# Main command for feature extration
+sox $inputfile -t raw -e signed -b 16 - | $X2X +sf | $FRAME -l 240 -p 80 | $WINDOW -l 400 -L 512 |
+	$MFCC -l 400 -m $mfcc_order -n $nfilter > $base.mfcc || exit 1
+   
+
+# Our array files need a header with the number of cols and rows:
+ncol=$((mfcc_order))  
+nrow=`$X2X +fa < $base.mfcc | wc -l | perl -ne 'print $_/'$ncol', "\n";'`
+
+# Build fmatrix file by placing nrow and ncol in front, and the data after them
+echo $nrow $ncol | $X2X +aI > $outputfile
+cat $base.mfcc >> $outputfile
