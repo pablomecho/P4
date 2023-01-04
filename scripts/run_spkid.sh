6ceb6f533b32d740e4e74474ad863aacbd1cb197
alexsoler9 <alex.soler.baca@estudiantat.upc.edu>
2023-01-04 21:02:24 +0100

    graphs

--- scripts/run_spkid.sh
+++ scripts/run_spkid.sh
@@ -25,7 +25,8 @@ db_test=spk_8mu/sr_test
 world=users_and_others
 
 #initial "-T 0.001 -N 5 -m 2"
-TRAIN_OPTS="-T 0.001 -N 32 -m 64"
+#"-i 0 -T 0.001 -N 32 -m 64"
+TRAIN_OPTS="-i 0 -T 0.001 -N 32 -m 64"
 
 
 #initial "-i 0 -T 0.0001 -N 10 -m 5"
@@ -142,7 +143,7 @@ for cmd in $*; do
        for dir in $db_devel/BLOCK*/SES* ; do
            name=${dir/*\/}
            echo $name ----
-           EXEC="gmm_train -v 2 $TRAIN_OPTS -d $w/$FEAT -e $FEAT -g $w/gmm/$FEAT/$name.gmm $lists/class/$name.train"
+           EXEC="gmm_train -v 1 $TRAIN_OPTS -d $w/$FEAT -e $FEAT -g $w/gmm/$FEAT/$name.gmm $lists/class/$name.train"
            echo $EXEC && $EXEC || exit 1
            echo
        done
@@ -168,7 +169,7 @@ for cmd in $*; do
        # Implement 'trainworld' in order to get a Universal Background Model for speaker verification
        #
        # - The name of the world model will be used by gmm_verify in the 'verify' command below.
-       EXEC="gmm_train -v 2 $WORLD_OPTS -d $w/$FEAT/ -e $FEAT -g $w/gmm/$FEAT/$world.gmm  $lists/verif/$world.train"
+       EXEC="gmm_train -v 1 $WORLD_OPTS -d $w/$FEAT/ -e $FEAT -g $w/gmm/$FEAT/$world.gmm  $lists/verif/$world.train"
        echo $EXEC && $EXEC || exit 1
 
    elif [[ $cmd == verify ]]; then
